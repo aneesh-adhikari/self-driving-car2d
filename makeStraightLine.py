da5505ad4a37ec8fde18df16b05988629a70bced
druvpatel <druvmp@gmail.com>
2017-04-18 16:11:23 -0500

    added druv neural net

---
+++ makeStraightLine.py
@@ -0,0 +1,8 @@
+import math
+f = open('line.txt', 'w+')
+def makeLine(y):
+    for i in range(0, 800, 5):
+        f.write(str(800-i) + " " + str(y))
+        f.write('\n')
+makeLine(400)
+makeLine(300)
