0408ad37609be648c12d938f22b39aa61678139d
druvpatel <druvmp@gmail.com>
2017-04-30 13:08:23 -0500

    axis labels and legend added to graphs

--- analysis.py
+++ analysis.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-
+mport matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 for i in range(0, 10):
 	f = open('avg' + str(i) + '.txt', 'r+')
@@ -16,7 +16,15 @@ for i in range(0, 10):
 		s = f.readline()
 		s2 = f2.readline()
 
-	plt.plot(range(0,len(avgs)), avgs, ls='-', lw = 2, c = 'r')
-	plt.plot(range(0,len(maxes)), maxes, ls='-', lw = 2, c = 'b')
+	avgs, = plt.plot(avgs, ls='-', lw = 2, c = 'r', label="Average Fitness")
+	bests, = plt.plot(maxes, ls='-', lw = 2, c = 'b', label="Best Fitness")
+
+	first_legend = plt.legend(handles=[bests], loc = 4, bbox_to_anchor=(1, .095))
+	ax = plt.gca().add_artist(first_legend)
+	plt.legend(handles=[avgs], loc=4, bbox_to_anchor=(1, 0))
+
+	plt.xlabel('Generation Number')
+	plt.ylabel('Fitness')
+
 
 	plt.show()
